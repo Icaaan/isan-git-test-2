0ee39cf523762e16c53eff7864f373f7c8751e59
Ichsan Maulana Putra <ichsanmaulanaputra09@gmail.com>
2019-05-20 10:46:06 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero voluptate esse consequatur maiores error consectetur, numquam sapiente veniam doloremque, facere atque. Eaque hic omnis iure, pariatur soluta temporibus dolorum atque.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
